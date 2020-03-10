66f9556bb50a066fbab56ad5c74532857660dace
Luciano <natielloluciano@gmail.com>
2020-03-09 23:18:28 -0300

    limpio rutas y archivos

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,6 @@ Route::view('/adminInicio', 'adminInicio')->middleware('auth', 'validarAdmin');
 /*************  USUARIOS ***********/ 
 Route::get('/adminUsuarios', 'UsuariosController@index')->middleware('auth', 'validarAdmin');
 Route::get('/borrarUsuario/{id}', 'UsuariosController@destroy')->middleware('auth', 'validarAdmin');
-/*Route::post('/busquedaUsuarios', 'UsuariosController@busquedaUsuarioAdmin')->middleware('auth', 'validarAdmin');*/
 Route::post('/agregarFavorito', 'UsuariosController@agregarFav');
 Route::post('/quitarFavorito', 'UsuariosController@quitarFav');
 Route::post('/quitarFavoritoUser', 'UsuariosController@quitarFavUser');
@@ -28,7 +27,7 @@ Route::post('/quitarFavoritoUser', 'UsuariosController@quitarFavUser');
 /********************* COMENTARIOSS *******************/
 Route::view('/comentario', 'comentario');
 Route::post('/comentario', 'UsuariosController@agregarComentario');
-Route::get('/verComentarios', 'DestinosController@verComentarios')->middleware('auth');
+
 Route::get('/verTodosComentarios', 'ComentariosController@todosComentarios')->middleware('auth');
 Route::get('/verComentariosDestino/{id}', 'DestinosController@verComentarios')->middleware('auth');
 
@@ -40,13 +39,12 @@ Route::post('/destinoAlta', "DestinosController@store")->middleware('auth', 'val
 Route::post('/destinoMod', "DestinosController@update")->middleware('auth', 'validarAdmin');
 Route::get('/destinoMod/{id}', 'DestinosController@edit')->middleware('auth', 'validarAdmin');
 Route::get('/borrarDestino/{id}', 'DestinosController@destroy')->middleware('auth', 'validarAdmin');
-Route::post('/busquedaDestinos', 'DestinosController@busquedaDestinoAdmin');
+
 
 
 /*---------------------usuariocomun-----------------------*/
 
 Route::get('/verDestino/{id}', 'DestinosController@verComentarios')->middleware('auth');
-
 Route::get('/destinos', 'DestinosController@pagDestinos');
 Route::get('/detalleDestino/{id}', 'DestinosController@detalleDestino')->middleware('auth');
 Route::post('busDestinosUser', 'DestinosController@busDestinosUser');
